0bd5e31d690e0fa53f2458154a1df26d686fcc95
Natalia <n.platova@hotmail.com>
2019-10-27 17:15:45 +0700

    Updates

---
+++ precache-manifest.4426f2b5585eaeb84cb593308952e0a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eff21e7153abebffa9e40a572c49cff5",
+    "url": "/med_ask/index.html"
+  },
+  {
+    "revision": "ff0cf915cb0b57b18e4c",
+    "url": "/med_ask/static/css/main.e5d61984.chunk.css"
+  },
+  {
+    "revision": "f043be921a045e4ba515",
+    "url": "/med_ask/static/js/2.87e6b1ed.chunk.js"
+  },
+  {
+    "revision": "ff0cf915cb0b57b18e4c",
+    "url": "/med_ask/static/js/main.7e7fba8d.chunk.js"
+  },
+  {
+    "revision": "157c35bf70a012bf3007",
+    "url": "/med_ask/static/js/runtime-main.f7e6811c.js"
+  }
+]);
